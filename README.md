ecac18551ea49f305adbb2c49a523acd0a7eb66e
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-10-12 12:09:00 -0500

    added cafe adding functionality

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 A website made with Python Flask and SQLite
 to find cafes with wifi.
 
+# Modules
+
+´´´
+Flask==2.0.2
+Werkzeug==2.0.2
+Flask-Login==0.5.0
+Flask-Bcrypt==0.7.1
+Flask-SQLAlchemy==2.5.1
+Flask-WTF==0.15.1
+´´´
+
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, request
 from flask.templating import render_template
+from flask_login.utils import login_required
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user
 from flask_bcrypt import Bcrypt, check_password_hash, generate_password_hash
 from werkzeug.utils import redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask("__name__")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
@@ -34,6 +39,19 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(), nullable=False)
 
 db.create_all()
+
+class CafeForm(FlaskForm):
+    name = StringField(label="Name", validators=[DataRequired()])
+    map_url = StringField(label="Google Maps URL", validators=[DataRequired()])
+    img_url = StringField(label="Image URL", validators=[DataRequired()])
+    location = StringField(label="Location", validators=[DataRequired()])
+    has_sockets = BooleanField(label="Has Sockets", validators=[DataRequired()])
+    has_toilet = BooleanField(label="Has Toilet", validators=[DataRequired()])
+    has_wifi = BooleanField(label="Has WiFi", validators=[DataRequired()])
+    can_take_calls = BooleanField(label="Can Take Calls", validators=[DataRequired()])
+    seats = StringField(label="Seats")
+    coffee_price = StringField(label="Coffee Price")
+    submit = SubmitField(label="Create")
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -103,6 +121,32 @@ def signup():
 
         return redirect("/")
     return render_template("signup.html", error = error)
+
+
+@login_required
+@app.route("/add_cafe", methods=["GET", "POST"])
+def add_cafe():
+    form = CafeForm()
+    if request.method == "POST":
+        if form.validate_on_submit():
+            new_cafe = Cafe(
+                name = form.name.data,
+                map_url = form.map_url.data,
+                img_url = form.img_url.data,
+                location = form.location.data,
+                has_sockets = form.has_sockets.data,
+                has_toilet = form.has_toilet.data,
+                has_wifi = form.has_wifi.data,
+                can_take_calls = form.can_take_calls.data,
+                seats = form.seats.data,
+                coffee_price = form.coffee_price.data
+            )
+
+            db.session.add(new_cafe)
+            db.session.commit()
+
+            return redirect("/")
+    return render_template("/add_cafe.html", form=form)
 
 @app.route("/logout")
 def logout():
